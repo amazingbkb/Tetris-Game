802a56fdd8fc4f7c7dc2d9d060da2552762dc9a2
Kevin Lin <lin.ke846@gmail.com>
2020-03-23 19:14:26 +0900

    Motified comments

--- js/TetrisBasic.js
+++ js/TetrisBasic.js
@@ -1,12 +1,25 @@
 let canvas;
 let ctx;
-let gBArrayHeight = 20; // Number of cells in array height
-let gBArrayWidth = 12; // Number of cells in array width
-let startX = 4; // Starting X position for Tetromino
-let startY = 0; // Starting Y position for Tetromino
-let score = 0; // Tracks the score
-let level = 1; // Tracks current level
+
+// Number of cells in array height
+let gBArrayHeight = 20; 
+
+// Number of cells in array width
+let gBArrayWidth = 12;
+
+// Starting X position for Tetromino
+let startX = 4; 
+
+// Starting Y position for Tetromino
+let startY = 0;
+
+// Tracks the score 
+let score = 0;
+
+// Tracks current level
+let level = 1; 
 let winOrLose = "Playing";
+
 // Used as a look up table where each value in the array
 // contains the x & y position we can use to draw the
 // box on the canvas
@@ -14,17 +27,17 @@ let coordinateArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth).fil
 
 let curTetromino = [[1,0], [0,1], [1,1], [2,1]];
 
-// 3. Will hold all the Tetrominos 
+// Will hold all the Tetrominos 
 let tetrominos = [];
-// 3. The tetromino array with the colors matched to the tetrominos array
+// The tetromino array with the colors matched to the tetrominos array
 let tetrominoColors = ['purple','cyan','blue','yellow','orange','green','red'];
-// 3. Holds current Tetromino color
+// Holds current Tetromino color
 let curTetrominoColor;
 
-// 4. Create gameboard array so we know where other squares are
+// Create gameboard array so we know where other squares are
 let gameBoardArray = [...Array(20)].map(e => Array(12).fill(0));
 
-// 6. Array for storing stopped shapes
+// Array for storing stopped shapes
 // It will hold colors when a shape stops and is added
 let stoppedShapeArray = [...Array(20)].map(e => Array(12).fill(0));
 
@@ -128,12 +141,12 @@ function SetupCanvas(){
     ctx.fillText("Down Arrow : Move Down", 310, 438);
     ctx.fillText("Space : Rotate Right", 310, 463);
 
-    // 2. Handle keyboard presses
+    //  Handle keyboard presses
     document.addEventListener('keydown', HandleKeyPress);
 
-    // 3. Create the array of Tetromino arrays
+    // Create the array of Tetromino arrays
     CreateTetrominos();
-    // 3. Generate random Tetromino
+    // Generate random Tetromino
     CreateTetromino();
 
     // Create the rectangle lookup table
@@ -156,7 +169,7 @@ function DrawTetromino(){
         let x = curTetromino[i][0] + startX;
         let y = curTetromino[i][1] + startY;
 
-        // 4. Put Tetromino shape in the gameboard array
+        // Put Tetromino shape in the gameboard array
         gameBoardArray[x][y] = 1;
         // console.log("Put 1 at [" + x + "," + y + "]");
 
@@ -167,22 +180,22 @@ function DrawTetromino(){
         // console.log("X : " + x + " Y : " + y);
         // console.log("Rect X : " + coordinateArray[x][y].x + " Rect Y : " + coordinateArray[x][y].y);
 
-        // 1. Draw a square at the x & y coordinates that the lookup
+        // Draw a square at the x & y coordinates that the lookup
         // table provides
         ctx.fillStyle = curTetrominoColor;
         ctx.fillRect(coorX, coorY, 21, 21);
     }
 }
 
-// ----- 2. Move & Delete Old Tetrimino -----
+// Eliminate old Tetromino
 // Each time a key is pressed we change the either the starting
 // x or y value for where we want to draw the new Tetromino
 // We also delete the previously drawn shape and draw the new one
 function HandleKeyPress(key){
     if(winOrLose != "Game Over"){
-    // a keycode (LEFT)
+    // keycode (LEFT)
     if(key.keyCode === 37){
-        // 4. Check if I'll hit the wall
+        // Check if I'll hit the wall
         direction = DIRECTION.LEFT;
         if(!HittingTheWall() && !CheckForHorizontalCollision()){
             DeleteTetromino();
@@ -190,10 +203,9 @@ function HandleKeyPress(key){
             DrawTetromino();
         } 
 
-    // d keycode (RIGHT)
+    // keycode (RIGHT)
     } else if(key.keyCode === 39){
         
-        // 4. Check if I'll hit the wall
         direction = DIRECTION.RIGHT;
         if(!HittingTheWall() && !CheckForHorizontalCollision()){
             DeleteTetromino();
@@ -201,10 +213,10 @@ function HandleKeyPress(key){
             DrawTetromino();
         }
 
-    // s keycode (DOWN)
+    // keycode (DOWN)
     } else if(key.keyCode === 40){
         MoveTetrominoDown();
-        // 9. e keycode calls for rotation of Tetromino
+        //  keycode (SPACE) calls for rotation of Tetromino
     } else if(key.keyCode === 32){
         RotateTetromino();
     }
@@ -212,10 +224,10 @@ function HandleKeyPress(key){
 }
 
 function MoveTetrominoDown(){
-    // 4. Track that I want to move down
+    // Track that I want to move down
     direction = DIRECTION.DOWN;
 
-    // 5. Check for a vertical collision
+    // Check for a vertical collision
     if(!CheckForVerticalCollison()){
         DeleteTetromino();
         startY++;
@@ -223,8 +235,7 @@ function MoveTetrominoDown(){
     }
 }
 
-// 10. Automatically calls for a Tetromino to fall every second
-
+//  Automatically calls for a Tetromino to fall every second
 window.setInterval(function(){
     if(winOrLose != "Game Over"){
         MoveTetrominoDown();
@@ -251,7 +262,7 @@ function DeleteTetromino(){
     }
 }
 
-// 3. Generate random Tetrominos with color
+// Generate random Tetrominos with color
 // We'll define every index where there is a colored block
 function CreateTetrominos(){
     // Push T 
@@ -279,7 +290,7 @@ function CreateTetromino(){
     curTetrominoColor = tetrominoColors[randomTetromino];
 }
 
-// 4. Check if the Tetromino hits the wall
+// Check if the Tetromino hits the wall
 // Cycle through the squares adding the upper left hand corner
 // position to see if the value is <= to 0 or >= 11
 // If they are also moving in a direction that would be off
@@ -296,7 +307,7 @@ function HittingTheWall(){
     return false;
 }
 
-// 5. Check for vertical collison
+// Check for vertical collison
 function CheckForVerticalCollison(){
     // Make a copy of the tetromino so that I can move a fake
     // Tetromino and check for collisions before I move the real
@@ -347,7 +358,7 @@ function CheckForVerticalCollison(){
             ctx.fillText(winOrLose, 310, 261);
         } else {
 
-            // 6. Add stopped Tetromino to stopped shape array
+            // Add stopped Tetromino to stopped shape array
             // so I can check for future collisions
             for(let i = 0; i < tetrominoCopy.length; i++){
                 let square = tetrominoCopy[i];
@@ -357,7 +368,7 @@ function CheckForVerticalCollison(){
                 stoppedShapeArray[x][y] = curTetrominoColor;
             }
 
-            // 7. Check for completed rows
+            // Check for completed rows
             CheckForCompletedRows();
 
             CreateTetromino();
@@ -372,7 +383,7 @@ function CheckForVerticalCollison(){
     }
 }
 
-// 6. Check for horizontal shape collision
+// Check for horizontal shape collision
 function CheckForHorizontalCollision(){
     // Copy the Teromino so I can manipulate its x value
     // and check if its new value would collide with
@@ -411,11 +422,10 @@ function CheckForHorizontalCollision(){
     return collision;
 }
 
-// 7. Check for completed rows
-// ***** SLIDE *****
+// Check for completed rows
 function CheckForCompletedRows(){
 
-    // 8. Track how many rows to delete and where to start deleting
+    // Track how many rows to delete and where to start deleting
     let rowsToDelete = 0;
     let startOfDeletion = 0;
 
@@ -471,7 +481,7 @@ function CheckForCompletedRows(){
     }
 }
 
-// 8. Move rows down after a row has been deleted
+// Move rows down after a row has been deleted
 function MoveAllRowsDown(rowsToDelete, startOfDeletion){
     for (var i = startOfDeletion-1; i >= 0; i--)
     {
@@ -505,8 +515,7 @@ function MoveAllRowsDown(rowsToDelete, startOfDeletion){
     }
 }
 
-// 9. Rotate the Tetromino
-// ***** SLIDE *****
+// Rotate the Tetromino
 function RotateTetromino()
 {
     let newRotation = new Array();
